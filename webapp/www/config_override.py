b5875e098df672c9173a9d524eb3ff28cd1f7bbf
wangwangqin523 <huangqiao2013@qq.com>
2019-07-31 23:41:23 +0800

    'Webapp'

--- webapp/www/config_override.py
+++ webapp/www/config_override.py
@@ -9,7 +9,7 @@ __author__ = 'Michael Liao'
 
 configs = {
     'db': {
-        'host': '127.0.0.1'
+        'host': '127.0.0.1',
 		'user': 'root',
         'password': 'password',
         'db': 'awesome'
